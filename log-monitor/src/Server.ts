4e18b27d37ef5ce742ee499e6e5ac44e19a71b11
Ronald van den Broek <ronald.vandenbroek@live.nl>
2020-01-19 19:34:42 +0100

    Adding JSDocs

--- log-monitor/src/Server.ts
+++ log-monitor/src/Server.ts
@@ -1,9 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 
+/**
+ * Server class used to recieve intercepted logs.
+ */
 class Server {
   public server;
 
+  /**
+ * Setup the express server.
+ */
   constructor() {
     this.server = express();
     this.server.use(express.json());
@@ -11,11 +17,14 @@ class Server {
     this.mountRoutes();
   }
 
+  /**
+  * Configure the CORS of the express server.
+  */
   private configureCORS(): void {
     this.server.use(cors());
     const whitelist = ['http://localhost:4200'];
     const corsOptions = {
-      origin: function (origin, callback) {
+      origin: function(origin, callback) {
         if (whitelist.indexOf(origin) !== -1) {
           callback(null, true);
         } else {
@@ -26,8 +35,11 @@ class Server {
     this.server.use(cors(corsOptions));
   }
 
+  /**
+  * Configuring the routes the express server can be called on.
+  */
   private mountRoutes(): void {
-    this.server.post('/log', function (request, response) {
+    this.server.post('/log', function(request, response) {
       const body = request.body['0'];
       console.log(body);
       response.status(200).send();
