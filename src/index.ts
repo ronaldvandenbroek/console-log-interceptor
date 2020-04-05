bf2a2dd66eec4a5dea6fa44645f2e3cbff9f71f9
Ronald van den Broek <ronald.vandenbroek@live.nl>
2020-04-05 16:17:38 +0200

    Adding Husky git-hooks

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-import { IMonitorCallback, LogMonitor } from './log-monitor';
+/* eslint no-unused-vars:
+  ["error", { "varsIgnorePattern": "IMonitorCallback" }] */
+import {IMonitorCallback, LogMonitor} from './log-monitor';
 
 // Port of the monitoring server.
 const port: number = 8888;
 // Array of domains the monitoring server should expect logs from.
 const whitelist: string[] = ['http://localhost:4200'];
 
-// The callback that can be implemented to process the logs in the rest of the application.
-let callback: IMonitorCallback = (log: string): void => {
+// The callback that can be implemented to process
+// the logs in the rest of the application.
+const callback: IMonitorCallback = (log: string): void => {
   console.log(log);
-}
+};
 
 // Create the monitoring server.
 new LogMonitor(callback, whitelist, port);
--- src/log-monitor.ts
+++ src/log-monitor.ts
@@ -15,32 +15,37 @@ export class LogMonitor {
   private logMonitor: express.Application;
 
   /**
- * Setup the express server.
- */
+   * Setup the express server.
+   * @param  {IMonitorCallback} callback
+   * @param  {string[]} whitelist
+   * @param  {number} port
+   */
   constructor(callback: IMonitorCallback, whitelist: string[], port: number) {
     this.setupExpress(port);
     this.configureCORS(whitelist);
     this.mountRoutes(callback);
-    callback(`Server started on port ${port}`)
+    callback(`Server started on port ${port}`);
   }
 
   /**
-  * Setup the the express server.
-  */
+   * Setup the the express server.
+   * @param  {number} port
+   */
   private setupExpress(port: number) {
     this.logMonitor = express();
     this.logMonitor.use(express.json());
-    this.logMonitor.listen(port)
+    this.logMonitor.listen(port);
   }
 
   /**
-  * Configure the CORS of the express server.
-  */
+   * Configure the CORS of the express server.
+   * @param  {string[]} whitelist
+   */
   private configureCORS(whitelist: string[]): void {
     if (whitelist) {
       const corsOptions: cors.CorsOptions = {
         origin: whitelist,
-        methods: "POST",
+        methods: 'POST',
       };
 
       this.logMonitor.use(cors(corsOptions));
@@ -48,8 +53,9 @@ export class LogMonitor {
   }
 
   /**
-  * Configuring the routes the express server can be called on.
-  */
+   * Configuring the routes the express server can be called on.
+   * @param  {IMonitorCallback} callback
+   */
   private mountRoutes(callback: IMonitorCallback): void {
     this.logMonitor.post('/log', function(request, response) {
       const body = request.body['0'];
